fdaadb3284d1a2531f1faf6e3731d374a1cd8253
kaykuhl <kakoreo4@gmail.com>
2024-12-27 23:57:47 -0600

    feat: add about page content

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div className="about">
+      <h1>About Everyday Tools Suite</h1>
+      <p>
+        Everyday Tools Suite is designed to provide simple, ad-free tools for everyday tasks. 
+        Our goal is to make your daily routines easier and more efficient.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
